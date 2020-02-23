0a53d7e80746437949fafe7edb8d4268f41bfe69
Tomasz Grzmilas <tomasz.grzmilas@gmail.com>
2020-02-23 19:56:18 +0100

    initial commit

---
+++ RdpFile.cs
@@ -0,0 +1,22 @@
+﻿namespace RdpHelper
+{
+    public class RdpFile
+    {
+        public static string Build(string address, string username = null, string password = null)
+        {
+            return InternalBuild(new RdpConfig(address, username, password));
+        }
+
+        public static string Build(in RdpConfig config)
+        {
+            return InternalBuild(config);
+        }
+
+        private static string InternalBuild(in RdpConfig config)
+        {
+            return config.ToString();
+        }
+    }
+}
+
+
